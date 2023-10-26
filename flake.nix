8797ee1f793cbb9952562cafcbbe69e7b09a566d
Mirko Lenz <mirko@mirkolenz.com>
2023-10-26 12:02:10 +0200

    build: switch to nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        pkgs,
+        system,
+        lib,
+        self',
+        ...
+      }: let
+        packages = with pkgs; [jdk11 gradle buf protobuf];
+      in {
+        packages = {
+          releaseEnv = pkgs.buildEnv {
+            name = "release-env";
+            paths = packages;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inherit packages;
+          shellHook = ''
+            ${lib.getExe pkgs.gradle} wrapper
+          '';
+        };
+      };
+    };
+}
