6c55d71ac5aa30cd6098b3ed8f5150d899c6e2c9
Mirko Lenz <mirko@mirkolenz.com>
2024-04-04 10:45:05 +0200

    ci: add release env with java

--- flake.nix
+++ flake.nix
@@ -4,37 +4,47 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
   };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-    systems,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = {
-        pkgs,
-        system,
-        lib,
-        self',
-        ...
-      }: {
-        packages = {
-          bufGenerate = pkgs.writeShellApplication {
-            name = "buf-generate";
-            text = ''
-              ${lib.getExe pkgs.buf} mod update
-              ${lib.getExe pkgs.buf} generate --include-imports buf.build/recap/arg-services
+      perSystem =
+        {
+          pkgs,
+          system,
+          lib,
+          self',
+          ...
+        }:
+        {
+          packages = {
+            releaseEnv = pkgs.buildEnv {
+              name = "release-env";
+              paths = with pkgs; [
+                jdk
+                gradle
+              ];
+            };
+            bufGenerate = pkgs.writeShellApplication {
+              name = "buf-generate";
+              text = ''
+                ${lib.getExe pkgs.buf} mod update
+                ${lib.getExe pkgs.buf} generate --include-imports buf.build/recap/arg-services
+              '';
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ gradle ];
+            shellHook = ''
+              ${lib.getExe pkgs.gradle} wrapper
             '';
           };
         };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [gradle];
-          shellHook = ''
-            ${lib.getExe pkgs.gradle} wrapper
-          '';
-        };
-      };
     };
 }
